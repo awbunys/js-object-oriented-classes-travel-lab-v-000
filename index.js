59390e5e905faf4c13e8cf9e503088ec7f5be7aa
Andrew Bunys <awbunys@gmail.com>
2018-08-01 08:39:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
+  yearsExperienceFromBeginningOf(year) {
+    let calcYear = this.startDate
+    let test = calcYear.getFullYear()
+    return (year - test - 1)
+  }
+}
 
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+  blocksTravelled() {
+    if(this.beginningLocation === this.endingLocation) {
+      return Math.abs(this.beginningLocation - this.endingLocation)
+    }
+  }
+}
